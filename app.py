1fc3f483ab897fd1e2e6e18a471d2d63b99621cb
Taeheon(Ted) Lee <68367134+taeheon7753@users.noreply.github.com>
2021-04-07 10:14:52 +0900

    코드 수정

--- app.py
+++ app.py
@@ -1,5 +1,38 @@
 # -*- conding: utf-8 -*-
-from flask_app import create_app
 
-app = create_app()
-app.run()
+# Deep Learning
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+
+# Flask
+import flask
+from flask import *
+
+
+def create_app():
+    app = flask.Flask(__name__)
+    app.config["DEBUG"] = True
+
+    # load model
+    model = load_model('oversampling_easy.h5')
+    model.call = tf.function(model.call)
+
+    # main index page route
+    @app.route('/')
+    def home():
+        return '<h1>API is working...!!! </h1>'
+
+    @app.route('/predict', methods=['POST'])
+    def predict():
+        data = np.array([request.get_json()])
+        prediction = model.predict_classes(data).tolist()
+        print('\n')
+        print('!!!!!!!!!!!!!!!data input success!!!!!!!!!!!!!!!')
+        print('\n')
+        return jsonify(prediction)
+
+    if __name__ == '__main__':
+        app.run(debug=True, host='0.0.0.0', port='5000')
+    
+    return app
